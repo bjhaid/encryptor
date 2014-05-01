26fa732eac577411804f919982ddaeb27a4ecc83
bjhaid <abejideayodele@gmail.com>
2014-05-01 12:25:57 -0500

    refractored magic number out of the method, and moved common logic to the initializer instance variable

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -1,14 +1,19 @@
 class Encryption
 
+  def initialize(seed_number = 13)
+    @rotated_lower_case_alphabets = ("a".."z").to_a.rotate(seed_number)
+    @rotated_upper_case_alphabets = ("A".."Z").to_a.rotate(seed_number)
+  end
+
   def encrypt(string)
     string.chars.map { |char| encrypt_char(char) }.join
   end
 
   def encrypt_char(char)
     if char.match(/[A-Z]/)
-      ("A".."Z").to_a.rotate(13)[char.ord - "A".ord]
+      @rotated_upper_case_alphabets[char.ord - "A".ord]
     elsif char.match(/[a-z]/)
-      ("a".."z").to_a.rotate(13)[char.ord - "a".ord]
+      @rotated_lower_case_alphabets[char.ord - "a".ord]
     else
       char
     end
